be8cd7138ef86ab6a84875ed26c6fd7bb1f2092e
nightmareyq <panpaphonwhk@gmail.com>
2026-02-10 19:23:46 +0700

    Add files via upload

--- java.js
+++ java.js
@@ -1,14 +1,6 @@
+
+
 document.getElementById("btn").addEventListener("click", function () {
-  document.getElementById("text").textContent = "กดปุ่มแล้วนะ 😊";
-});
-
-document.getElementById("btn1").addEventListener("click", function () {
-  this.textContent = "ถูกกดแล้ว!";
-});
-document.getElementById("btn2").addEventListener("click", function () {
-  this.style.color = "red";
-});
-document.getElementById("nextBtn").addEventListener("click", function () {
   window.location.href = "page2.html";
+  window.location.replace("page2.html");
 });
-
